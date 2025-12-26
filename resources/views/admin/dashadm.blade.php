84d8b22c26d44dda2484b39ccd894dffdf527559
si-eja <fauzan57889@gmail.com>
2025-12-26 13:09:57 +0700

    push

--- resources/views/admin/dashadm.blade.php
+++ resources/views/admin/dashadm.blade.php
@@ -2,7 +2,30 @@
 @section('content')
 <section class="container-fluid bg-light" style="height: 100vh;">
     <div class="container py-5">
-        <h1 class="fw-bold">Dashboard Admin Tasty Food</h1>
+        <div class="row">
+            <div class="col-md-7">
+                <div class="row g-2">
+                    <div class="col-md-6">
+                        <div class="rounded bg-light shadow p-3"></div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="rounded bg-light shadow p-3"></div>
+                    </div>
+                </div>
+                <div class="my-2">
+                    <div class="rounded bg-light shadow p-3" style="height: 200px;"></div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="rounded bg-light shadow p-3" style="height: 100%;"></div>
+            </div>
+        </div>
+        <div class="mt-2">
+            <div class="rounded p-3 bg-light shadow"></div>
+        </div>
+        <div class="mt-2">
+            <div class="rounded p-3 bg-light shadow"></div>
+        </div>
     </div>
 </section>
 @endsection
